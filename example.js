29ecbc655f27f10278458b088ee4c9f6f8ab24f1
Victor Kelvin <victorkelvin@gmail.com>
2022-02-21 20:44:18 -0300

    Add 'example.csv' in this repo

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ let adults = 2;
 let children = 1;
 let checkin = "2022-05-20";
 let checkout = "2022-05-25";
-let CSVFile = './test.csv';
+let CSVFile = './example.csv';
 
 
 scraper.scrape(region, adults, children, checkin, checkout).then((scrapedData) => {
